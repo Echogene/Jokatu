0c797dbf43be5886daebb59ac6ec7ed942fc92ff
Steven Weston <echogene.alpha@gmail.com>
2015-07-09 18:59:14 +0100

    Rename variable

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -32,23 +32,23 @@ function simpleXhrSentinel(xhr) {
 }
 
 function verifyAssertion(assertion) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "/login/persona", true);
+	var request = new XMLHttpRequest();
+	request.open("POST", "/login/persona", true);
 	var param = "assertion=" + assertion;
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xhr.setRequestHeader("Content-length", param.length);
-	xhr.setRequestHeader("Connection", "close");
-	xhr.setRequestHeader(_csrf_header, _csrf);
-	xhr.send(param);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.setRequestHeader("Content-length", param.length);
+	request.setRequestHeader("Connection", "close");
+	request.setRequestHeader(_csrf_header, _csrf);
+	request.send(param);
 
-	xhr.onreadystatechange = simpleXhrSentinel(xhr);
+	request.onreadystatechange = simpleXhrSentinel(request);
 }
 
 function signoutUser() {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "/xhr/sign-out", true);
-	xhr.send(null);
-	xhr.onreadystatechange = simpleXhrSentinel(xhr);
+	var request = new XMLHttpRequest();
+	request.open("GET", "/request/sign-out", true);
+	request.send(null);
+	request.onreadystatechange = simpleXhrSentinel(request);
 }
 
 navigator.id.watch({
