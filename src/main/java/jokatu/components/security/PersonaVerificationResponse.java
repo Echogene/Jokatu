a1c03eddc4ebb4213b4bd4c8c3fefaa929c572f7
Steven Weston <echogene.alpha@gmail.com>
2015-07-09 18:59:38 +0100

    Map the JSON fields in the constructor

--- src/main/java/jokatu/components/security/PersonaVerificationResponse.java
+++ src/main/java/jokatu/components/security/PersonaVerificationResponse.java
@@ -1,6 +1,9 @@
 package jokatu.components.security;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
+ * {"audience":"http://localhost:8080","expires":1436464604148,"issuer":"gmail.login.persona.org","email":"steven.weston.alpha@gmail.com","status":"okay"}
  * @author Steven Weston
  */
 public class PersonaVerificationResponse {
@@ -9,7 +12,11 @@ public class PersonaVerificationResponse {
 	public final String status;
 	public final String reason;
 
-	public PersonaVerificationResponse(String email, String status, String reason) {
+	public PersonaVerificationResponse(
+			@JsonProperty("email") String email,
+			@JsonProperty("status") String status,
+			@JsonProperty("reason") String reason
+	) {
 		this.email = email;
 		this.status = status;
 		this.reason = reason;
