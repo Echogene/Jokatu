6fb80ef54b70843ec11b7ee5eaae568e69d599cc
Steven Weston <echogene.alpha@gmail.com>
2015-07-14 22:47:04 +0100

    I should make up my mind

--- src/main/java/jokatu/game/user/player/Player.java
+++ src/main/java/jokatu/game/user/player/Player.java
@@ -1,10 +1,8 @@
 package jokatu.game.user.player;
 
-import ophelia.event.observable.Observable;
-
 /**
  * @author Steven Weston
  */
-public interface Player<E> extends Observable<E> {
+public interface Player {
 
 }
