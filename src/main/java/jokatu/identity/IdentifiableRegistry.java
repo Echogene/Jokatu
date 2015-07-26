0c0be3962787e364df861430047722b3593b3f80
Echogene <echogene.alpha@gmail.com>
2015-07-26 11:39:55 +0100

    Doc

--- src/main/java/jokatu/identity/IdentifiableRegistry.java
+++ src/main/java/jokatu/identity/IdentifiableRegistry.java
@@ -1,6 +1,7 @@
 package jokatu.identity;
 
 /**
+ * A place to store {@link Identifiable} things.
  * @author Steven Weston
  */
 public interface IdentifiableRegistry<I extends Identity, J extends Identifiable<I>> {
