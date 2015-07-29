51f8470540768aa50428897fb214f52efc3d96ef
Steven Weston <echogene.alpha@gmail.com>
2015-07-29 20:39:23 +0100

    Send messages through STOMP when requesting event

--- src/main/resources/static/js/game_main.js
+++ src/main/resources/static/js/game_main.js
@@ -1,5 +1,15 @@
 var ws = new WebSocket('ws://' + window.location.host + '/ws');
 
+ws.onmessage = function(e) {
+	var div = document.getElementById('lol');
+	if (!div) {
+		div = document.createElement('div');
+		div.id = 'lol';
+		document.body.appendChild(div);
+	}
+	div.innerHTML = e.data;
+};
+
 ws.onopen = function() {
 	ws.send(subscribe('/game/' + game.identifier));
 };
