02be65debbe39dec70b5de33d601a19f57ec9676
Steven Weston <echogene.alpha@gmail.com>
2015-07-31 18:29:34 +0100

    Throw when input is unacceptable

--- src/main/java/jokatu/game/Game.java
+++ src/main/java/jokatu/game/Game.java
@@ -1,6 +1,7 @@
 package jokatu.game;
 
 import jokatu.game.input.Input;
+import jokatu.game.input.UnacceptableInputException;
 import jokatu.game.joining.CannotJoinGameException;
 import jokatu.game.user.player.Player;
 import jokatu.identity.Identifiable;
@@ -18,5 +19,5 @@ public interface Game<P extends Player, I extends Input<P>> extends Identifiable
 
 	Status getStatus();
 
-	void accept(I input);
+	void accept(I input) throws UnacceptableInputException;
 }
---
+++ src/main/java/jokatu/game/input/UnacceptableInputException.java
@@ -0,0 +1,8 @@
+package jokatu.game.input;
+
+public class UnacceptableInputException extends Exception {
+
+	public UnacceptableInputException(String message) {
+		super(message);
+	}
+}
