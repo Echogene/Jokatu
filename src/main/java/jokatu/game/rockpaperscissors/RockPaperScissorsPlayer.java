fc4bae3da2c6aabbce1b2b5bc5b37ab3bb232f65
Steven Weston <echogene.alpha@gmail.com>
2015-08-03 21:04:32 +0100

    Give the player a name

--- src/main/java/jokatu/game/rockpaperscissors/RockPaperScissorsPlayer.java
+++ src/main/java/jokatu/game/rockpaperscissors/RockPaperScissorsPlayer.java
@@ -3,4 +3,14 @@ package jokatu.game.rockpaperscissors;
 import jokatu.game.user.player.Player;
 
 public class RockPaperScissorsPlayer implements Player {
+
+	private final String name;
+
+	public RockPaperScissorsPlayer(String username) {
+		this.name = username;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
