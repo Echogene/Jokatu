88dd821e59d0ee0da8bf58f2e198aaeff53ae7de
Steven Weston <echogene.alpha@gmail.com>
2015-08-03 21:06:38 +0100

    Add a player factory for RPS players

---
+++ src/main/java/jokatu/game/rockpaperscissors/RockPaperScissorsPlayerFactory.java
@@ -0,0 +1,15 @@
+package jokatu.game.rockpaperscissors;
+
+import jokatu.game.factory.Factory;
+import jokatu.game.factory.player.PlayerFactory;
+import org.jetbrains.annotations.NotNull;
+
+import static jokatu.game.rockpaperscissors.RockPaperScissorsGame.ROCK_PAPER_SCISSORS;
+
+@Factory(gameName = ROCK_PAPER_SCISSORS)
+public class RockPaperScissorsPlayerFactory implements PlayerFactory<RockPaperScissorsPlayer> {
+	@Override
+	public RockPaperScissorsPlayer produce(@NotNull String username) {
+		return new RockPaperScissorsPlayer(username);
+	}
+}
