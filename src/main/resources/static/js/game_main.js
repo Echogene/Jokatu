da12eddba05d359354aabacb9c55499375316b8e
Echogene <echogene.alpha@gmail.com>
2015-08-19 20:01:00 +0100

    Reorder

--- src/main/resources/static/js/game_main.js
+++ src/main/resources/static/js/game_main.js
@@ -1,6 +1,10 @@
 var socket = new Socket();
 socket.subscribe(`/game/${game.identifier}`, handleMessage);
 
+function join() {
+	post('/joinGame.do', {gameID: game.identifier}, (game) => alert(JSON.stringify(game)));
+}
+
 function handleMessage(e) {
 	var div = document.getElementById('lol');
 	if (!div) {
@@ -9,10 +13,6 @@ function handleMessage(e) {
 		document.body.appendChild(div);
 	}
 	div.innerHTML = JSON.stringify(e);
-}
-
-function join() {
-	post('/joinGame.do', {gameID: game.identifier}, (game) => alert(JSON.stringify(game)));
 }
 
 function choose() {
