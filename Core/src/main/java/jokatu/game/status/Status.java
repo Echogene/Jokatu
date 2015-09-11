15769822e0e3730cabb9e94da6e60a9119cb76c0
Echogene <echogene.alpha@gmail.com>
2015-09-11 23:47:53 +0100

    Document

--- Core/src/main/java/jokatu/game/status/Status.java
+++ Core/src/main/java/jokatu/game/status/Status.java
@@ -3,6 +3,7 @@ package jokatu.game.status;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * The status of a game should be displayed to all observers.  One example would be ‘Waiting for players to join’.
  * @author steven
  */
 public interface Status {
