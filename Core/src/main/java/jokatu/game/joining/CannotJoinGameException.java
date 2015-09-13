b13f8bb32a6821aa965cd13a1f60a0cfb837b45c
Echogene <echogene.alpha@gmail.com>
2015-09-13 21:12:24 +0100

    Document

--- Core/src/main/java/jokatu/game/joining/CannotJoinGameException.java
+++ Core/src/main/java/jokatu/game/joining/CannotJoinGameException.java
@@ -3,6 +3,9 @@ package jokatu.game.joining;
 import jokatu.game.GameID;
 import jokatu.game.exception.GameException;
 
+/**
+ * Thrown to say that a player cannot join a game for some reason.
+ */
 public class CannotJoinGameException extends GameException {
 
 	public CannotJoinGameException(GameID id, String message) {
