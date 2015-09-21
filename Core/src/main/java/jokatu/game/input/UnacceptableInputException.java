13c1da17a1ff7fa6a68d5f8b6acf84b796e4cbc0
Echogene <echogene.alpha@gmail.com>
2015-09-21 21:23:48 +0100

    Document

--- Core/src/main/java/jokatu/game/input/UnacceptableInputException.java
+++ Core/src/main/java/jokatu/game/input/UnacceptableInputException.java
@@ -3,6 +3,11 @@ package jokatu.game.input;
 import jokatu.game.GameID;
 import jokatu.game.exception.GameException;
 
+/**
+ * An exception thrown when a user's input is not acceptable.  This could mean that the user sent badly-formed input or
+ * they sent it at the wrong time.  If one of these is thrown, it is probably an indication that either the frontend
+ * has a bug that sends an input or something is tampering with the user's requests.
+ */
 public class UnacceptableInputException extends GameException {
 
 	public UnacceptableInputException(GameID id, String message) {
