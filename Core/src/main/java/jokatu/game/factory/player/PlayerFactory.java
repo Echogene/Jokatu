9acf3f601c95ea09de8df3ca742d028b95161ec0
Echogene <echogene.alpha@gmail.com>
2015-09-27 14:51:45 +0100

    Document

--- Core/src/main/java/jokatu/game/factory/player/PlayerFactory.java
+++ Core/src/main/java/jokatu/game/factory/player/PlayerFactory.java
@@ -3,6 +3,10 @@ package jokatu.game.factory.player;
 import jokatu.game.player.Player;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Given a username, this constructs a player for its respective game.
+ * @param <P>
+ */
 public interface PlayerFactory<P extends Player> {
 
 	@NotNull P produce(@NotNull String username);
