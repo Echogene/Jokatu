572b1b39a1f2bd1c15621c7c398027a2a533185c
Echogene <echogene.alpha@gmail.com>
2015-10-03 16:15:35 +0100

    Document

--- Core/src/main/java/jokatu/game/factory/input/InputDeserialiser.java
+++ Core/src/main/java/jokatu/game/factory/input/InputDeserialiser.java
@@ -3,6 +3,10 @@ package jokatu.game.factory.input;
 import jokatu.game.input.Input;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This takes JSON from the client and turns it into input.
+ * @param <I>
+ */
 public interface InputDeserialiser<I extends Input> {
 
 	@NotNull
