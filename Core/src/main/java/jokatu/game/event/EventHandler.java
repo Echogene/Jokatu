319cd7879b9fbcffed02c2d1b2ccfed3b6d78e46
Echogene <echogene.alpha@gmail.com>
2015-10-31 08:40:44 +0000

    Document

--- Core/src/main/java/jokatu/game/event/EventHandler.java
+++ Core/src/main/java/jokatu/game/event/EventHandler.java
@@ -1,8 +1,10 @@
 package jokatu.game.event;
 
 import jokatu.game.Game;
+import org.springframework.stereotype.Component;
 
 /**
+ * An event handler that is annotated with {@link Component} is passed events when they are emitted from a game.
  * @author steven
  */
 public interface EventHandler {
