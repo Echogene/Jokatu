039c6d94aba2988eaac4a4d661ba2862e6472ebe
Echogene <echogene.alpha@gmail.com>
2015-11-07 15:31:00 +0000

    Document

--- Core/src/main/java/jokatu/game/player/Player.java
+++ Core/src/main/java/jokatu/game/player/Player.java
@@ -3,6 +3,7 @@ package jokatu.game.player;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * A player is a user that participates in a game.
  * @author Steven Weston
  */
 public interface Player {
