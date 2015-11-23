76ba3f232ec806cccfabbb976c40eef560b3c164
Echogene <echogene.alpha@gmail.com>
2015-11-23 12:01:10 +0000

    Use an abstract class like view resolver

--- Core/src/main/java/jokatu/game/viewresolver/ViewResolverFactory.java
+++ Core/src/main/java/jokatu/game/viewresolver/ViewResolverFactory.java
@@ -4,11 +4,30 @@ import jokatu.game.Game;
 import jokatu.game.player.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.text.MessageFormat;
+
 /**
  * @author steven
  */
-public interface ViewResolverFactory<P extends Player, G extends Game<P, ?>> {
+public abstract class ViewResolverFactory<P extends Player, G extends Game<P, ?>> {
 
 	@NotNull
-	ViewResolver<P, G> getViewResolver(Game<?, ?> game);
+	public final ViewResolver<P, G> getViewResolver(Game<?, ?> game) {
+		G castGame = castGame(game);
+		return getResolverFor(castGame);
+	}
+
+	private G castGame(Game<?, ?> game) {
+		Class<G> gameClass = handlesGame();
+		if (!gameClass.isInstance(game)) {
+			throw new IllegalArgumentException(
+					MessageFormat.format("Game was not of the right type.  Expected {0}", gameClass)
+			);
+		}
+		return gameClass.cast(game);
+	}
+
+	protected abstract Class<G> handlesGame();
+
+	protected abstract ViewResolver<P, G> getResolverFor(G castGame);
 }
