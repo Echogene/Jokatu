644a2af32d582c1fe6c0ba8f9cfbe6bd7c194f11
Echogene <echogene.alpha@gmail.com>
2015-11-23 11:23:11 +0000

    Add view resolver interface

---
+++ Core/src/main/java/jokatu/game/viewresolver/ViewResolver.java
@@ -0,0 +1,40 @@
+package jokatu.game.viewresolver;
+
+import jokatu.game.Game;
+import jokatu.game.player.Player;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.MessageFormat;
+
+/**
+ * @author steven
+ */
+public abstract class ViewResolver<P extends Player, G extends Game<P, ?>> {
+
+	private final G game;
+
+	protected ViewResolver(G game) {
+		this.game = game;
+	}
+
+	public ModelAndView getViewForPlayer(Player player) {
+		P castPlayer = castPlayer(player);
+		return getViewFor(castPlayer);
+	}
+
+	public abstract ModelAndView getViewForObserver();
+
+	private P castPlayer(Player player) {
+		Class<P> playerClass = handlesPlayer();
+		if (!playerClass.isInstance(player)) {
+			throw new IllegalArgumentException(
+					MessageFormat.format("Player was not of the right type.  Expected {0}", playerClass)
+			);
+		}
+		return playerClass.cast(player);
+	}
+
+	protected abstract Class<P> handlesPlayer();
+
+	protected abstract ModelAndView getViewFor(P player);
+}
