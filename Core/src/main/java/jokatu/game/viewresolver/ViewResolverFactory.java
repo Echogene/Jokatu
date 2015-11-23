e45c6d6f200087994f6fad22f4e49281015c00cc
Echogene <echogene.alpha@gmail.com>
2015-11-23 11:43:15 +0000

    Specify not null

--- Core/src/main/java/jokatu/game/viewresolver/ViewResolverFactory.java
+++ Core/src/main/java/jokatu/game/viewresolver/ViewResolverFactory.java
@@ -2,11 +2,13 @@ package jokatu.game.viewresolver;
 
 import jokatu.game.Game;
 import jokatu.game.player.Player;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author steven
  */
 public interface ViewResolverFactory<P extends Player, G extends Game<P, ?>> {
 
+	@NotNull
 	ViewResolver<P, G> getViewResolver(Game<?, ?> game);
 }
