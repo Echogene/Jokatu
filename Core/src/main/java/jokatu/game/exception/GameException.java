42a6e7ae3a3861f45fb104e171ac3f01aa679a52
Echogene <echogene.alpha@gmail.com>
2016-03-22 17:01:39 +0000

    Add shortcut constructor using message format

--- Core/src/main/java/jokatu/game/exception/GameException.java
+++ Core/src/main/java/jokatu/game/exception/GameException.java
@@ -2,6 +2,8 @@ package jokatu.game.exception;
 
 import jokatu.game.GameID;
 
+import java.text.MessageFormat;
+
 /**
  * An exception that occurs within a particular game.
  * @author steven
@@ -13,6 +15,10 @@ public class GameException extends Exception {
 	public GameException(GameID id, String message) {
 		super(message);
 		this.id = id;
+	}
+
+	public GameException(GameID id, String pattern, Object... arguments) {
+		this(id, MessageFormat.format(pattern, arguments));
 	}
 
 	public GameID getId() {
