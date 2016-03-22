c0d6f33ef160302fcbd04b8cf3d43bf856ca5e2c
Echogene <echogene.alpha@gmail.com>
2016-03-22 08:45:51 +0000

    Allow deserialising to map

--- src/main/groovy/jokatu/util/Json.java
+++ src/main/groovy/jokatu/util/Json.java
@@ -1,21 +1,31 @@
 package jokatu.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A utility class for handling JSON.
  */
 public class Json {
 
-	public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
 	public static String serialise(Object object) throws JsonProcessingException {
 		return OBJECT_MAPPER.writeValueAsString(object);
+	}
+
+	public static Map<String, Object> deserialise(String json) throws IOException {
+		MapType typeRef = OBJECT_MAPPER.getTypeFactory().constructMapType(HashMap.class, String.class, Object.class);
+		return OBJECT_MAPPER.readValue(json, typeRef);
 	}
 
 	public static class JacksonConverterFactory implements ConverterFactory<String, Object> {
