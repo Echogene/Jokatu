c1b0fc2c70b76d9f8e2fad43ce6242ddc3f8de55
Echogene <echogene.alpha@gmail.com>
2016-03-25 16:24:54 +0000

    Make these not extend game exception

--- Core/src/main/java/jokatu/game/joining/CannotJoinGameException.java
+++ Core/src/main/java/jokatu/game/joining/CannotJoinGameException.java
@@ -1,14 +1,11 @@
 package jokatu.game.joining;
 
-import jokatu.game.GameID;
-import jokatu.game.exception.GameException;
-
 /**
  * Thrown to say that a player cannot join a game for some reason.
  */
-public class CannotJoinGameException extends GameException {
+public class CannotJoinGameException extends Exception {
 
-	public CannotJoinGameException(GameID id, String message) {
-		super(id, message);
+	public CannotJoinGameException(String message) {
+		super(message);
 	}
 }
