621c383baf2bc4a5fc9afe2a459f4ef80601f5df
Steven Weston <echogene.alpha@gmail.com>
2016-03-30 18:20:59 +0100

    Annotate overriding methods too

--- Games/EchoGame/src/main/java/jokatu/game/games/echo/game/EchoGame.java
+++ Games/EchoGame/src/main/java/jokatu/game/games/echo/game/EchoGame.java
@@ -48,18 +48,21 @@ public class EchoGame extends Game<EchoPlayer> {
 		return inputAcceptors;
 	}
 
+	@org.jetbrains.annotations.NotNull
 	@NotNull
 	@Override
 	public String getGameName() {
 		return ECHO;
 	}
 
+	@org.jetbrains.annotations.NotNull
 	@NotNull
 	@Override
 	public BaseCollection<EchoPlayer> getPlayers() {
 		return emptySet();
 	}
 
+	@org.jetbrains.annotations.NotNull
 	@NotNull
 	@Override
 	public Status getStatus() {
