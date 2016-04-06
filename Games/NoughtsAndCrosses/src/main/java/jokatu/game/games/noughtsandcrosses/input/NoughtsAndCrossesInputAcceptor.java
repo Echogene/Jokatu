47c5f2f6d2764e9bb8dda876b8fd7e346fc6a770
Steven Weston <echogene.alpha@gmail.com>
2016-04-06 20:53:57 +0100

    Fire events when lines are completed

--- Games/NoughtsAndCrosses/src/main/java/jokatu/game/games/noughtsandcrosses/input/NoughtsAndCrossesInputAcceptor.java
+++ Games/NoughtsAndCrosses/src/main/java/jokatu/game/games/noughtsandcrosses/input/NoughtsAndCrossesInputAcceptor.java
@@ -1,20 +1,40 @@
 package jokatu.game.games.noughtsandcrosses.input;
 
 import jokatu.game.games.noughtsandcrosses.event.CellChosenEvent;
+import jokatu.game.games.noughtsandcrosses.event.LineCompletedEvent;
 import jokatu.game.games.noughtsandcrosses.player.NoughtsAndCrossesPlayer;
 import jokatu.game.input.InputAcceptor;
 import jokatu.game.status.StandardTextStatus;
+import ophelia.collections.list.UnmodifiableList;
+import ophelia.collections.set.HashSet;
+import ophelia.collections.set.UnmodifiableSet;
 import ophelia.collections.set.bounded.BoundedPair;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
 import static jokatu.game.games.noughtsandcrosses.input.NoughtOrCross.CROSS;
 import static jokatu.game.games.noughtsandcrosses.input.NoughtOrCross.other;
 
 public class NoughtsAndCrossesInputAcceptor extends InputAcceptor<NoughtsAndCrossesInput, NoughtsAndCrossesPlayer> {
 
 	private final Map<Integer, NoughtOrCross> inputs = new HashMap<>();
+
+	private static final UnmodifiableSet<UnmodifiableList<Integer>> LINES;
+	static {
+		Set<UnmodifiableList<Integer>> lines = new HashSet<>();
+		IntStream.range(0, 3)
+				.map(i -> i * 3)
+				.forEach(i -> lines.add(new UnmodifiableList<>(asList(i, i + 1, i + 2))));
+		IntStream.range(0, 3)
+				.forEach(i -> lines.add(new UnmodifiableList<>(asList(i, i + 3, i + 6))));
+		lines.add(new UnmodifiableList<>(asList(0, 4, 8)));
+		lines.add(new UnmodifiableList<>(asList(2, 4, 6)));
+		LINES = new UnmodifiableSet<>(lines);
+	}
 
 	public NoughtsAndCrossesInputAcceptor(BoundedPair<NoughtsAndCrossesPlayer> players, StandardTextStatus status) {
 		super();
@@ -39,5 +59,25 @@ public class NoughtsAndCrossesInputAcceptor extends InputAcceptor<NoughtsAndCros
 			inputs.put(cell, CROSS);
 		}
 		fireEvent(new CellChosenEvent(cell, inputs.get(cell)));
+		fireLineCompletedEvents();
+	}
+
+	private void fireLineCompletedEvents() {
+		stream(NoughtOrCross.values()).forEach(this::fireLineCompletedEventsForPlayer);
+	}
+
+	private void fireLineCompletedEventsForPlayer(NoughtOrCross noughtOrCross) {
+		Set<Integer> cellsForPlayer = getCellsForPlayer(noughtOrCross);
+		LINES.stream()
+				.filter(line -> line.stream().allMatch(cellsForPlayer::contains))
+				.map(LineCompletedEvent::new)
+				.forEach(this::fireEvent);
+	}
+
+	private Set<Integer> getCellsForPlayer(NoughtOrCross noughtOrCross) {
+		return inputs.entrySet().stream()
+				.filter(entry -> entry.getValue() == noughtOrCross)
+				.map(Map.Entry::getKey)
+				.collect(Collectors.toSet());
 	}
 }
