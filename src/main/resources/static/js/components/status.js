008f162d4f943c1844815e15463901a27040a011
Steven Weston <echogene.alpha@gmail.com>
2016-04-07 17:42:54 +0100

    Allow status to accept an array of statuses

--- src/main/resources/static/js/components/status.js
+++ src/main/resources/static/js/components/status.js
@@ -2,30 +2,52 @@ var JStatusProto = Object.create(HTMLElement.prototype);
 
 JStatusProto.createdCallback = function() {
 
-	var statusElementName = this.getAttribute('wrapperElement');
+	this._statusElementName = this.getAttribute('wrapperElement');
 	var destination = this.getAttribute('destination');
 
-	/**
-	 * @type {Element}
-	 * @private
-	 */
-	this._element = document.createElement(statusElementName);
+	this._shadowRoot = this.createShadowRoot();
 
 	var initialData = JSON.parse(this.getAttribute('data-initial'));
 	this._setStatus(initialData);
 
 	socket.subscribe(destination, this._setStatus.bind(this));
-
-	this.createShadowRoot().appendChild(this._element);
 };
 
-JStatusProto._setStatus = function(body) {
-	if (typeof body === 'string') {
-		this._element.textContent = body;
-	} else if (body && typeof body === 'object' && typeof body.text === 'string') {
-		this._element.textContent = body.text;
+JStatusProto._setStatus = function(statuses) {
+	if (!(statuses instanceof Array)) {
+		statuses = [statuses];
 	}
-	this._element.setAttribute('data-status', JSON.stringify(body));
+	this._ensureEnoughElements(statuses.length);
+	for (var i = 0; i < this._shadowRoot.childNodes.length; i++) {
+		var element = this._shadowRoot.childNodes[i];
+		var status = statuses[i];
+		if (status) {
+			this._updateElement(element, status);
+		} else {
+			// Hide the element.
+			element.style.display = 'none';
+		}
+	}
+};
+
+JStatusProto._ensureEnoughElements = function(number) {
+	var elementsNeeded = number - this._shadowRoot.childNodes.length;
+	if (elementsNeeded > 0) {
+		for (var i = 0; i < elementsNeeded; i++) {
+			this._shadowRoot.appendChild(document.createElement(this._statusElementName));
+		}
+	}
+};
+
+JStatusProto._updateElement = function(element, status) {
+	// Show the element.
+	element.style.display = '';
+	if (typeof status === 'string') {
+		element.textContent = status;
+	} else if (status && typeof status === 'object' && typeof status.text === 'string') {
+		element.textContent = status.text;
+	}
+	element.setAttribute('data-status', JSON.stringify(status));
 };
 
 var JStatus = document.registerElement('j-status', {
