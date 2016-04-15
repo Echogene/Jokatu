d18633549466abfc534ae49e3876381cf843301c
Steven Weston <echogene.alpha@gmail.com>
2016-04-15 22:30:02 +0100

    Don't deal with errors in game_main

--- src/main/resources/static/js/game_main.js
+++ src/main/resources/static/js/game_main.js
@@ -1,5 +1,4 @@
 var socket = new Socket();
-socket.subscribe(`/user/topic/errors.game.${game.identifier}`, handleError);
 socket.subscribe(`/user/topic/private.game.${game.identifier}`, handleMessage);
 
 function handleMessage(e) {
@@ -10,23 +9,4 @@ function handleMessage(e) {
 		document.body.appendChild(div);
 	}
 	div.innerHTML = JSON.stringify(e);
-}
-
-/**
- * @param {Object} e
- * @param {Map=} headers
- */
-function handleError(e, headers) {
-	var div = document.getElementById('error');
-	if (!div) {
-		div = document.createElement('div');
-		div.id = 'error';
-		document.body.appendChild(div);
-	}
-	if (headers && headers.has('subscription-id')) {
-		div.innerHTML = 'Subscription error: ' + JSON.stringify(e.message);
-		socket.unsubscribe(headers.get('subscription-id'));
-	} else {
-		div.innerHTML = 'Unknown error: ' + JSON.stringify(e.message);
-	}
 }
