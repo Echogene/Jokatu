f9a365fa402130f1b9d5da3c598aa9fbdcb34b2d
Steven Weston <echogene.alpha@gmail.com>
2016-04-16 10:50:56 +0100

    Improve stacktrace and add typing

--- src/main/resources/static/js/components/errormessage.js
+++ src/main/resources/static/js/components/errormessage.js
@@ -1,3 +1,13 @@
+/**
+ * @typedef {{
+ *     message: string,
+ *     localisedMessage: string,
+ *     stackTrace: Array
+ *     cause: Error|null
+ * }}
+ */
+Error;
+
 var JErrorMessageProto = Object.create(HTMLButtonElement.prototype);
 
 JErrorMessageProto.createdCallback = function() {
@@ -22,15 +32,36 @@ JErrorMessageProto._updateMessage = function() {
 };
 
 JErrorMessageProto._showStacktrace = function() {
+	/**
+	 * @type Error
+	 */
 	var error = JSON.parse(this.getAttribute('data-message'));
 	if (!error) {
 		return;
 	}
-	var stacktrace = "";
-	error.stackTrace.forEach(el => {
-		stacktrace += `at ${el.className}.${el.methodName}(${el.fileName}:${el.lineNumber})\n`
-	});
+
+	var causes = 0;
+	var stacktrace = '';
+	do {
+		if (causes > 0) {
+			stacktrace += `Caused by:\n`
+		}
+		stacktrace += `${error.message} \n`;
+		stacktrace += this._getStackTrace(error);
+	} while ((error = error.cause) && (causes++ < 10));
+
 	alert(stacktrace);
+};
+
+/**
+ * @param {Error} error
+ * @returns {string}
+ * @private
+ */
+JErrorMessageProto._getStackTrace = function(error) {
+	return error.stackTrace.reduce((previousValue, el) => {
+		return previousValue + `\tat ${el.className}.${el.methodName}(${el.fileName}:${el.lineNumber})\n`
+	}, "");
 };
 
 var JErrorMessage = document.registerElement('j-error', {
