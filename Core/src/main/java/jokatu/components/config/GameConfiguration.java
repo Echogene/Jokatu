ef51afd0493229251d9607de81a425c10bde5f0d
Steven Weston <echogene.alpha@gmail.com>
2016-05-19 23:20:34 +0100

    Doc

--- Core/src/main/java/jokatu/components/config/GameConfiguration.java
+++ Core/src/main/java/jokatu/components/config/GameConfiguration.java
@@ -8,19 +8,33 @@ import ophelia.collections.BaseCollection;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * Groups together all the necessary components for a type of game.
  * @author steven
  */
 public interface GameConfiguration {
 
+	/**
+	 * @return the {@link GameFactory} that will be used to construct games of this type
+	 */
 	@NotNull
 	GameFactory getGameFactory();
 
+	/**
+	 * @return the {@link PlayerFactory} that will be used to create players for games of this type
+	 */
 	@NotNull
 	PlayerFactory getPlayerFactory();
 
+	/**
+	 * @return the {@link InputDeserialiser}s that will be used to deserialise client inputs for games of this type
+	 */
 	@NotNull
 	BaseCollection<? extends InputDeserialiser> getInputDeserialisers();
 
+	/**
+	 * @return the {@link ViewResolverFactory} that will be used to determine the views that the client should use for
+	 *         games of this type
+	 */
 	@NotNull
 	ViewResolverFactory getViewResolverFactory();
 }
