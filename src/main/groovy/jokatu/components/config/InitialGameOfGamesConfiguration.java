c12c0cc792dd41b9ddee4abef13e4302931b2b2c
Steven Weston <echogene.alpha@gmail.com>
2016-05-23 21:01:04 +0100

    Don't depend on all the game factories

--- src/main/groovy/jokatu/components/config/InitialGameOfGamesConfiguration.java
+++ src/main/groovy/jokatu/components/config/InitialGameOfGamesConfiguration.java
@@ -1,14 +1,12 @@
 package jokatu.components.config;
 
 import jokatu.components.eventhandlers.EventBroadcaster;
-import jokatu.game.Game;
-import jokatu.game.GameFactory;
+import jokatu.game.games.gameofgames.game.GameOfGames;
+import jokatu.game.games.gameofgames.game.GameOfGamesFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
-
-import static jokatu.game.games.gameofgames.game.GameOfGames.GAME_OF_GAMES;
 
 /**
  * Create the game of games.
@@ -17,20 +15,18 @@ import static jokatu.game.games.gameofgames.game.GameOfGames.GAME_OF_GAMES;
 public class InitialGameOfGamesConfiguration {
 
 	@Autowired
-	private FactoryConfiguration.GameFactories gameFactories;
+	private GameOfGamesFactory factory;
 
 	@Autowired
 	private EventBroadcaster eventBroadcaster;
 
 	@PostConstruct
 	public void createGameOfGames() {
-		GameFactory factory = gameFactories.getFactory(GAME_OF_GAMES);
+		GameOfGames theGameOfGames = factory.produceGame("");
 
-		Game<?> game = factory.produceGame("");
-
-		game.observe(event -> eventBroadcaster.broadcast(game, event));
+		theGameOfGames.observe(event -> eventBroadcaster.broadcast(theGameOfGames, event));
 
 		// Now we are observing events, start the game.
-		game.advanceStage();
+		theGameOfGames.advanceStage();
 	}
 }
