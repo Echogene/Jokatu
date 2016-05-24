290a0918400e266c6bc3ba72d698717283adf8df
Steven Weston <echogene.alpha@gmail.com>
2016-05-24 21:40:24 +0100

    Add getters and a generated label

--- Core/src/main/java/jokatu/game/cards/Card.java
+++ Core/src/main/java/jokatu/game/cards/Card.java
@@ -2,9 +2,12 @@ package jokatu.game.cards;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import java.text.MessageFormat;
+
 public class Card {
 
 	private final String display;
+	private final String label;
 	private final Rank rank;
 	private final Suit suit;
 
@@ -12,11 +15,25 @@ public class Card {
 		this.display = display;
 		this.rank = rank;
 		this.suit = suit;
+
+		label = MessageFormat.format(
+				"{0} of {1}",
+				rank.toString().toLowerCase(),
+				suit.toString().toLowerCase()
+		);
 	}
 
 	@JsonValue
 	@Override
 	public String toString() {
 		return display;
+	}
+
+	public Rank getRank() {
+		return rank;
+	}
+
+	public Suit getSuit() {
+		return suit;
 	}
 }
