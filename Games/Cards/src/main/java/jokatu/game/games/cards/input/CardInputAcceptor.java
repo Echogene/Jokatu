d7bd7aaa0874cf0c745e0c7d0fcb6aad0c8677ca
Steven Weston <echogene.alpha@gmail.com>
2016-05-24 21:17:07 +0100

    Start implementing game logic

--- Games/Cards/src/main/java/jokatu/game/games/cards/input/CardInputAcceptor.java
+++ Games/Cards/src/main/java/jokatu/game/games/cards/input/CardInputAcceptor.java
@@ -4,34 +4,34 @@ import jokatu.game.cards.Card;
 import jokatu.game.cards.StandardCards;
 import jokatu.game.games.cards.player.CardPlayer;
 import jokatu.game.input.InputAcceptor;
+import jokatu.game.input.UnacceptableInputException;
 import jokatu.game.status.StandardTextStatus;
+import ophelia.collections.set.HashSet;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import static java.text.MessageFormat.format;
 import static java.util.Collections.shuffle;
+import static jokatu.game.cards.StandardCards.SEVEN_OF_DIAMONDS;
 
 public class CardInputAcceptor extends InputAcceptor<CardInput, CardPlayer> {
 
 	private final List<CardPlayer> players;
 	private final StandardTextStatus status;
 
-	private final List<Card> deck;
+	private final Set<Card> extremeCards = new HashSet<>();
+
+	private CardPlayer currentPlayer;
 
 	public CardInputAcceptor(Map<String, CardPlayer> players, StandardTextStatus status) {
 		this.players = assignDealOrder(players);
-		// Forward events from players.
-		this.players.forEach(player -> player.observe(this::fireEvent));
 		this.status = status;
 
-		deck = StandardCards.getNewDeck();
-		shuffle(deck);
-
 		dealHands();
-
-		status.setText("Waiting for the owner of the seven of diamonds.");
 	}
 
 	private List<CardPlayer> assignDealOrder(Map<String, CardPlayer> players) {
@@ -39,10 +39,16 @@ public class CardInputAcceptor extends InputAcceptor<CardInput, CardPlayer> {
 	}
 
 	private void dealHands() {
+		List<Card> deck = StandardCards.getNewDeck();
+		shuffle(deck);
 		for (int i = 0; i < deck.size(); i++) {
 			Card card = deck.get(i);
 			CardPlayer player = players.get(i % players.size());
 			player.drawCard(card);
+			if (card == SEVEN_OF_DIAMONDS) {
+				currentPlayer = player;
+				status.setText(format("Waiting for the {0} to play the seven of diamonds.", currentPlayer));
+			}
 		}
 	}
 
@@ -60,6 +66,29 @@ public class CardInputAcceptor extends InputAcceptor<CardInput, CardPlayer> {
 
 	@Override
 	protected void acceptCastInputAndPlayer(@NotNull CardInput input, @NotNull CardPlayer inputter) throws Exception {
+		Card card = input.getCard();
+		if (currentPlayer != inputter) {
+			throw new UnacceptableInputException("It''s not your turn!  Wait for {0}.", currentPlayer);
+		}
+		if (!inputter.getHand().contains(card)) {
+			throw new UnacceptableInputException("Stop cheating!  You can't play a card that's not in your hand.");
+		}
+		if (hasAdjacentExtremeCard(card)) {
+			inputter.playCard(card);
+		}
 
+		completeTurn();
+	}
+
+	private boolean hasAdjacentExtremeCard(@NotNull Card card) {
+		if (extremeCards.isEmpty()) {
+			return card == SEVEN_OF_DIAMONDS;
+		}
+		return false;
+	}
+
+	private void completeTurn() {
+		int i = players.indexOf(currentPlayer);
+		currentPlayer = players.get((i + 1) % players.size());
 	}
 }
