66ac41c7ca48c89b88986055e5e12aec160ae648
Steven Weston <echogene.alpha@gmail.com>
2016-05-29 16:31:49 +0100

    Remove multi input stage for echo

--- Games/EchoGame/src/main/java/jokatu/game/games/echo/game/EchoStage.java
+++ Games/EchoGame/src/main/java/jokatu/game/games/echo/game/EchoStage.java
@@ -1,11 +1,35 @@
 package jokatu.game.games.echo.game;
 
-import jokatu.game.MultiInputStage;
-import jokatu.game.games.echo.input.EchoInputAcceptor;
+import jokatu.game.event.AbstractPrivateGameEvent;
+import jokatu.game.games.echo.input.EchoInput;
+import jokatu.game.input.InputAcceptor;
+import jokatu.game.player.StandardPlayer;
+import org.jetbrains.annotations.NotNull;
 
-class EchoStage extends MultiInputStage {
+import java.util.Collections;
 
-	EchoStage() {
-		super(new EchoInputAcceptor());
+class EchoStage extends InputAcceptor<EchoInput, StandardPlayer> {
+	@NotNull
+	@Override
+	protected Class<EchoInput> getInputClass() {
+		return EchoInput.class;
+	}
+
+	@NotNull
+	@Override
+	protected Class<StandardPlayer> getPlayerClass() {
+		return StandardPlayer.class;
+	}
+
+	@Override
+	protected void acceptCastInputAndPlayer(@NotNull EchoInput input, @NotNull StandardPlayer inputter) throws Exception {
+		fireEvent(new Echo(input, inputter));
+		fireEvent(new AbstractPrivateGameEvent(Collections.singleton(inputter)) {
+			@NotNull
+			@Override
+			public String getMessage() {
+				return "You said: " + input.getString();
+			}
+		});
 	}
 }
