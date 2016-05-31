037c6ab56013a400610e7b04bc307eb8b7337e05
Steven Weston <echogene.alpha@gmail.com>
2016-05-31 22:36:21 +0100

    Document attributes

--- src/main/resources/static/js/components/status.js
+++ src/main/resources/static/js/components/status.js
@@ -101,6 +101,21 @@ JStatusProto._updateElement = function(element, status) {
  * When it has spawned/hidden the appropriate children, it passes the corresponding array element into each child's
  * <code>data-status</code> attribute and updates the child's text content if the array element is a string or is an
  * object with a <code>text</code> string field.
+ *
+ * Attributes:<ul>
+ *     <li><code>destination</code>: the STOMP destination to subscribe to.</li>
+ *     <li><code>wrapperElement</code>: the name of the element to use as the child elements.  This should be the tag
+ *         name for standard elements and the JavaScript class name for custom elements.</li>
+ *     <li><code>data-wrapperAttributes</code>: when creating a child element, an object to use whose keys and values
+ *         will be the attributes and values to set on the child element.</li>
+ *     <li><code>attributeName</code>: an attribute name to set on each child element with the status for that element.
+ *         </li>
+ *     <li><code>attributeWrapper</code>: a JSON string where <code>$status</code> will be replaced with the status
+ *         for that element.  The resulting JSON string will be used as the value for <code>attributeName</code>.</li>
+ *     <li><code>data-initial</code>: an object for the initial data for the status.</li>
+ *     <li><code>showAll</code>: instead of deleting or hiding children, keep them around but clear their text.</li>
+ *     <li><code>removeOldChildren</code>: instead of hiding or emptying children, delete the children.</li>
+ * </ul>
  */
 var JStatus = document.registerElement('j-status', {
 	prototype: JStatusProto
