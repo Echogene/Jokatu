1b225652a11eb826f137f856e1910899ea58d1c4
Steven Weston <echogene.alpha@gmail.com>
2016-05-31 11:26:33 +0100

    Rename and document field

--- Core/src/main/java/jokatu/game/stage/JoiningStage.java
+++ Core/src/main/java/jokatu/game/stage/JoiningStage.java
@@ -12,20 +12,26 @@ import java.util.Map;
 public class JoiningStage<P extends Player> extends InputAcceptor<JoinInput, P> {
 
 	private final Class<P> playerClass;
+
 	private final Map<String, P> players;
-	private final int limit;
+
+	/**
+	 * The number of players that need to join the game needs before it starts.
+	 */
+	private final int number;
+
 	private final StandardTextStatus status;
 
-	public JoiningStage(Class<P> playerClass, Map<String, P> players, int limit, StandardTextStatus status) {
+	public JoiningStage(Class<P> playerClass, Map<String, P> players, int number, StandardTextStatus status) {
 		this.playerClass = playerClass;
 		this.players = players;
-		this.limit = limit;
+		this.number = number;
 		this.status = status;
 
 		status.setText(
 				"Waiting for {0} player{1} to join.",
-				limit,
-				limit == 1 ? "" : "s"
+				number,
+				number == 1 ? "" : "s"
 		);
 	}
 
@@ -46,10 +52,10 @@ public class JoiningStage<P extends Player> extends InputAcceptor<JoinInput, P> 
 	protected void acceptCastInputAndPlayer(@NotNull JoinInput input, @NotNull P inputter) throws Exception {
 		checkCanJoin(inputter);
 		players.put(inputter.getName(), inputter);
-		if (players.size() == limit) {
+		if (players.size() == number) {
 			fireEvent(new StageOverEvent());
 		} else {
-			int more = limit - players.size();
+			int more = number - players.size();
 			status.setText(
 					"Waiting for {0} more player{1} to join.",
 					more,
@@ -60,7 +66,7 @@ public class JoiningStage<P extends Player> extends InputAcceptor<JoinInput, P> 
 	}
 
 	private void checkCanJoin(P inputter) throws CannotJoinGameException {
-		if (players.size() > limit - 1) {
+		if (players.size() > number - 1) {
 			throw new GameFullException("No more players can join.");
 		}
 		if (players.containsKey(inputter.getName())) {
