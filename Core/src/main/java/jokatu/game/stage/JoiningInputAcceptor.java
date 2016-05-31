b33bed89119a309a296743baae421d7631f88d22
Steven Weston <echogene.alpha@gmail.com>
2016-05-31 11:49:38 +0100

    Don't control the status from the acceptor

--- Core/src/main/java/jokatu/game/stage/JoiningInputAcceptor.java
+++ Core/src/main/java/jokatu/game/stage/JoiningInputAcceptor.java
@@ -4,7 +4,6 @@ import jokatu.game.event.StageOverEvent;
 import jokatu.game.input.InputAcceptor;
 import jokatu.game.joining.*;
 import jokatu.game.player.Player;
-import jokatu.game.status.StandardTextStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
@@ -20,21 +19,11 @@ public class JoiningInputAcceptor<P extends Player> extends InputAcceptor<JoinIn
 	 */
 	private final int number;
 
-	private final StandardTextStatus status;
-
-	public JoiningInputAcceptor(@NotNull Class<P> playerClass, @NotNull Map<String, P> players, int number, @NotNull StandardTextStatus status) {
+	public JoiningInputAcceptor(@NotNull Class<P> playerClass, @NotNull Map<String, P> players, int number) {
 		this.playerClass = playerClass;
 		this.players = players;
 		this.number = number;
-		this.status = status;
-
-		status.setText(
-				"Waiting for {0} player{1} to join.",
-				number,
-				number == 1 ? "" : "s"
-		);
 	}
-
 
 	@NotNull
 	@Override
@@ -54,13 +43,6 @@ public class JoiningInputAcceptor<P extends Player> extends InputAcceptor<JoinIn
 		players.put(inputter.getName(), inputter);
 		if (players.size() == number) {
 			fireEvent(new StageOverEvent());
-		} else {
-			int more = number - players.size();
-			status.setText(
-					"Waiting for {0} more player{1} to join.",
-					more,
-					more == 1 ? "" : "s"
-			);
 		}
 		fireEvent(new PlayerJoinedEvent(inputter));
 	}
--- Core/src/main/java/jokatu/game/stage/JoiningStage.java
+++ Core/src/main/java/jokatu/game/stage/JoiningStage.java
@@ -8,7 +8,26 @@ import java.util.Map;
 
 public class JoiningStage<P extends Player> extends JoiningInputAcceptor<P> {
 
+	private final StandardTextStatus status;
+
 	public JoiningStage(@NotNull Class<P> playerClass, @NotNull Map<String, P> players, int number, @NotNull StandardTextStatus status) {
-		super(playerClass, players, number, status);
+		super(playerClass, players, number);
+
+		this.status = status;
+
+		status.setText(
+				"Waiting for {0} player{1} to join.",
+				number,
+				number == 1 ? "" : "s"
+		);
+
+		observe(gameEvent -> {
+			int more = number - players.size();
+			status.setText(
+					"Waiting for {0} more player{1} to join.",
+					more,
+					more == 1 ? "" : "s"
+			);
+		});
 	}
 }
