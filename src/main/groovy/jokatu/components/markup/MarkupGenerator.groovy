bab29553c3033f34a53136e792acfdf2549d64b5
Steven Weston <echogene.alpha@gmail.com>
2016-06-01 13:26:14 +0100

    Extract several methods

--- src/main/groovy/jokatu/components/markup/MarkupGenerator.groovy
+++ src/main/groovy/jokatu/components/markup/MarkupGenerator.groovy
@@ -12,44 +12,58 @@ class MarkupGenerator {
 	MessageRepository messageRepository
 
 	String bindHistory(Map properties) {
-		String destination = properties.destination
-
-		def initialData = Json.serialise(messageRepository.getMessageHistory(destination));
-
-		def tag = properties.remove("tag")
-
-		return "<${tag} ${properties.inject("", {s, k, v -> "${s}${k}='${v}' "})} data-initial='${initialData}'></${tag}>"
+		return generateTagMarkup(properties, getHistory(properties));
 	}
 
 	String bindUserHistory(Map properties) {
-		String destination = properties.destination.replaceFirst("^/user", "")
-
-		String username = properties.remove("user")
-		def initialData = Json.serialise(messageRepository.getMessageHistoryForUser(username, destination));
-
-		def tag = properties.remove("tag")
-
-		return "<${tag} ${properties.inject("", {s, k, v -> "${s}${k}='${v}' "})} data-initial='${initialData}'></${tag}>"
+		return generateTagMarkup(properties, getUserHistory(properties));
 	}
 
 	String bindLast(Map properties) {
-		String destination = properties.destination
-
-		def initialData = Json.serialise(messageRepository.getLastMessage(destination));
-
-		def tag = properties.remove("tag")
-
-		return "<${tag} ${properties.inject("", {s, k, v -> "${s}${k}='${v}' "})} data-initial='${initialData}'></${tag}>"
+		return generateTagMarkup(properties, getLast(properties));
 	}
 
 	String bindUserLast(Map properties) {
+		return generateTagMarkup(properties, getUserLast(properties));
+	}
+
+	private String getHistory(Map properties) {
+		String destination = properties.destination
+
+		return Json.serialise(messageRepository.getMessageHistory(destination));
+	}
+
+	private String getUserHistory(Map properties) {
 		String destination = properties.destination.replaceFirst("^/user", "")
 
 		String username = properties.remove("user")
-		def initialData = Json.serialise(messageRepository.getLastMessageForUser(username, destination));
+		return Json.serialise(messageRepository.getMessageHistoryForUser(username, destination));
+	}
 
+	private String getLast(Map properties) {
+		String destination = properties.destination
+
+		return Json.serialise(messageRepository.getLastMessage(destination));
+	}
+
+	private String getUserLast(Map properties) {
+		String destination = properties.destination.replaceFirst("^/user", "")
+
+		String username = properties.remove("user")
+		return Json.serialise(messageRepository.getLastMessageForUser(username, destination));
+	}
+
+	private static GString generateTagMarkup(Map properties, initialData) {
 		def tag = properties.remove("tag")
 
-		return "<${tag} ${properties.inject("", {s, k, v -> "${s}${k}='${v}' "})} data-initial='${initialData}'></${tag}>"
+		return generateStartTag(tag, properties, initialData) + generateEndTag(tag)
+	}
+
+	private static GString generateStartTag(tag, Map properties, initialData) {
+		"<${tag} ${properties.inject("", { s, k, v -> "${s}${k}='${v}' " })} data-initial='${initialData}'>"
+	}
+
+	private static GString generateEndTag(tag) {
+		"</${tag}>"
 	}
 }
