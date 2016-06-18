6d5c9533232ded4ec680b454d0c2b2fd7203f783
Steven Weston <echogene.alpha@gmail.com>
2016-06-18 20:45:26 +0100

    Doc

--- Core/src/main/java/jokatu/stomp/SendErrorMessage.java
+++ Core/src/main/java/jokatu/stomp/SendErrorMessage.java
@@ -3,6 +3,9 @@ package jokatu.stomp;
 import ophelia.util.MapUtils;
 import org.springframework.messaging.support.ErrorMessage;
 
+/**
+ * An error message returned to the client when one of its SEND commands is rejected.
+ */
 public class SendErrorMessage extends ErrorMessage {
 	public SendErrorMessage(Throwable e, String receipt) {
 		super(e, MapUtils.createMap("receipt-id", receipt));
