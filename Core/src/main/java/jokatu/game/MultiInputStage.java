19d27e7b4b684b034231776396b4a62f2b6ee157
Steven Weston <echogene.alpha@gmail.com>
2016-06-18 20:41:40 +0100

    Doc and use interface instead of abstract class

--- Core/src/main/java/jokatu/game/MultiInputStage.java
+++ Core/src/main/java/jokatu/game/MultiInputStage.java
@@ -2,7 +2,7 @@ package jokatu.game;
 
 import jokatu.game.event.GameEvent;
 import jokatu.game.input.Input;
-import jokatu.game.input.AbstractInputAcceptor;
+import jokatu.game.input.InputAcceptor;
 import jokatu.game.player.Player;
 import jokatu.game.stage.Stage;
 import ophelia.event.observable.AbstractSynchronousObservable;
@@ -14,11 +14,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * A stage that can accept multiple types of {@link Input}s by using some {@link InputAcceptor}s
+ */
 public abstract class MultiInputStage extends AbstractSynchronousObservable<GameEvent> implements Stage<GameEvent> {
 
-	private final List<AbstractInputAcceptor<? extends Input, ? extends Player, ? extends GameEvent>> inputAcceptors = new ArrayList<>();
+	private final List<InputAcceptor<? extends GameEvent>> inputAcceptors = new ArrayList<>();
 
-	protected void addInputAcceptor(AbstractInputAcceptor<? extends Input, ? extends Player, ? extends GameEvent> inputAcceptor) {
+	protected void addInputAcceptor(InputAcceptor<? extends GameEvent> inputAcceptor) {
 		inputAcceptors.add(inputAcceptor);
 		inputAcceptor.observe(this::fireEvent);
 	}
