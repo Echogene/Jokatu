86927a2af937ab7578748fca4a3180fda1e19f8c
Steven Weston <echogene.alpha@gmail.com>
2016-07-10 21:48:19 +0100

    Just create new things in constructor

--- Games/Uzta/src/main/java/jokatu/game/games/uzta/graph/ModifiableUztaGraph.java
+++ Games/Uzta/src/main/java/jokatu/game/games/uzta/graph/ModifiableUztaGraph.java
@@ -5,17 +5,14 @@ import ophelia.collections.set.HashSet;
 import ophelia.graph.ModifiableBiGraph;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-import java.util.Set;
-
 public class ModifiableUztaGraph implements UztaGraph, ModifiableBiGraph<Node, LineSegment> {
 
 	protected final ArrayList<Node> nodes;
 	protected final HashSet<LineSegment> edges;
 
-	public ModifiableUztaGraph(@NotNull List<Node> nodes, @NotNull Set<LineSegment> edges) {
-		this.nodes = new ArrayList<>(nodes);
-		this.edges = new HashSet<>(edges);
+	public ModifiableUztaGraph() {
+		this.nodes = new ArrayList<>();
+		this.edges = new HashSet<>();
 	}
 
 	@NotNull
