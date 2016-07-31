82ee606caca5940e8a5d3853c80b06aa78e91e61
Steven Weston <echogene.alpha@gmail.com>
2016-07-31 15:55:32 +0100

    Add singular and plural to node type

--- Games/Uzta/src/main/java/jokatu/game/games/uzta/graph/NodeType.java
+++ Games/Uzta/src/main/java/jokatu/game/games/uzta/graph/NodeType.java
@@ -1,7 +1,24 @@
 package jokatu.game.games.uzta.graph;
 
+import org.jetbrains.annotations.NotNull;
+
 public enum NodeType {
-	SQUARE,
-	CIRCLE,
-	RHOMBUS
+	SQUARE("square", "squares"),
+	CIRCLE("circle", "circles"),
+	RHOMBUS("rhombus", "rhombi");
+
+	@NotNull
+	private final String singular;
+	@NotNull
+	private final String plural;
+
+	NodeType(@NotNull String singular, @NotNull String plural) {
+		this.singular = singular;
+		this.plural = plural;
+	}
+
+	@NotNull
+	public String getNumber(int number) {
+		return number == 1 ? singular : plural;
+	}
 }
