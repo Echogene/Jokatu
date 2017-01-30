ee3c9309369aac39b38cbc66dd1dc597dd71d4f5
Steven Weston <echogene.alpha@gmail.com>
2017-01-30 21:59:43 +0000

    Throw an error if the player gets into debt

--- Games/Uzta/src/main/java/jokatu/game/games/uzta/player/UztaPlayer.java
+++ Games/Uzta/src/main/java/jokatu/game/games/uzta/player/UztaPlayer.java
@@ -5,12 +5,17 @@ import jokatu.game.games.uzta.event.ResourcesUpdatedEvent;
 import jokatu.game.games.uzta.game.UztaColour;
 import jokatu.game.games.uzta.graph.NodeType;
 import jokatu.game.player.Player;
+import ophelia.collections.bag.BagUtils;
 import ophelia.collections.bag.BaseIntegerBag;
 import ophelia.collections.bag.HashBag;
 import ophelia.collections.bag.ModifiableIntegerBag;
 import ophelia.event.observable.AbstractSynchronousObservable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.text.MessageFormat;
+
+import static java.util.stream.Collectors.joining;
 
 public class UztaPlayer extends AbstractSynchronousObservable<GameEvent> implements Player {
 
@@ -33,6 +38,18 @@ public class UztaPlayer extends AbstractSynchronousObservable<GameEvent> impleme
 	}
 
 	public void giveResources(@NotNull BaseIntegerBag<NodeType> givenResources) {
+		if (resources.getSum(givenResources).hasLackingItems()) {
+			throw new NotEnoughResourcesException(
+					"{0} does not have enough resources {1}",
+					name,
+					BagUtils.presentBag(
+							givenResources,
+							NodeType::getNumber,
+							joining(", ", "to be given ", " "),
+							joining(", ", "and to give ", ".")
+					)
+			);
+		}
 		resources.merge(givenResources);
 		fireEvent(new ResourcesUpdatedEvent(this, givenResources));
 	}
@@ -55,5 +72,11 @@ public class UztaPlayer extends AbstractSynchronousObservable<GameEvent> impleme
 
 	public int getNumberOfType(@NotNull NodeType type) {
 		return resources.getNumberOf(type);
+	}
+
+	public static class NotEnoughResourcesException extends RuntimeException {
+		public NotEnoughResourcesException(@NotNull String pattern, Object... arguments) {
+			super(MessageFormat.format(pattern, arguments));
+		}
 	}
 }
