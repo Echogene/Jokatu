491068a16dbdb299caeb77e05f86922fceea8ef1
Steven Weston <echogene.alpha@gmail.com>
2017-02-22 22:30:50 +0000

    Make event handler for calculating score

---
+++ Games/Uzta/src/main/java/jokatu/game/games/uzta/event/ScoreUpdatedEventHandler.java
@@ -0,0 +1,51 @@
+package jokatu.game.games.uzta.event;
+
+import jokatu.game.event.AbstractEventHandler;
+import jokatu.game.games.uzta.game.Uzta;
+import jokatu.game.games.uzta.graph.LineSegment;
+import jokatu.game.games.uzta.graph.UztaGraph;
+import jokatu.game.games.uzta.player.UztaPlayer;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+
+import static java.util.function.Predicate.isEqual;
+
+@Component
+public class ScoreUpdatedEventHandler extends AbstractEventHandler<Uzta, GraphUpdatedEvent> {
+	@NotNull
+	@Override
+	protected Class<GraphUpdatedEvent> getEventClass() {
+		return GraphUpdatedEvent.class;
+	}
+
+	@NotNull
+	@Override
+	protected Class<Uzta> getGameClass() {
+		return Uzta.class;
+	}
+
+	@Override
+	protected void handleCastGameAndEvent(
+			@NotNull Uzta game, @NotNull GraphUpdatedEvent event
+	) {
+		game.getPlayers().forEach(
+				player -> sender.send(
+						MessageFormat.format(
+								"/topic/score.game.{0}.{1}",
+								game.getIdentifier(),
+								player.getName()
+						),
+						calculateScore(player, game.getGraph())
+				)
+		);
+	}
+
+	private long calculateScore(@NotNull UztaPlayer player, @NotNull UztaGraph graph) {
+		return graph.getEdges().stream()
+				.map(LineSegment::getOwner)
+				.filter(isEqual(player))
+				.count();
+	}
+}
