9a9d103d01b291f441388ee1de850318c36e5863
Steven Weston <echogene.alpha@gmail.com>
2017-03-09 21:34:22 +0000

    Doc

--- Games/Uzta/src/main/java/jokatu/game/games/uzta/event/GraphUpdatedEvent.java
+++ Games/Uzta/src/main/java/jokatu/game/games/uzta/event/GraphUpdatedEvent.java
@@ -3,6 +3,9 @@ package jokatu.game.games.uzta.event;
 import jokatu.game.event.GameEvent;
 import ophelia.graph.Graph;
 
+/**
+ * An event fired when a graph is updated.
+ */
 public class GraphUpdatedEvent implements GameEvent {
 	private final Graph<?, ?> graph;
 
