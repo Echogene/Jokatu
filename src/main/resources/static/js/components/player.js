0e4c41482d906c43dba132fa5259119925de835d
Echogene <echogene.alpha@gmail.com>
2023-09-26 02:21:12 +0100

    Fix user status
    the field is 'isOnline', not 'online'

--- src/main/resources/static/js/components/player.js
+++ src/main/resources/static/js/components/player.js
@@ -1,10 +1,6 @@
 class JPlayer extends HTMLElement {
 	constructor() {
 		super();
-
-		observeAttributes(this, new Map([
-			['data-player', this._updateStatus.bind(this)]
-		]));
 	};
 
 	connectedCallback() {
@@ -16,21 +12,31 @@ class JPlayer extends HTMLElement {
 			this._status = this._content.querySelector('.status');
 
 			this.appendChild(this._content);
+
+			observeAttributes(this, new Map([
+				['data-player', this._updateStatus.bind(this)]
+			]));
 		}
 		this._alreadySetUp = true;
 	}
 
-	_updateStatus(status) {
-		if (!status) {
+	/**
+	 * @param player
+	 * @param {string} player.name
+	 * @param {boolean} player.isOnline
+	 * @private
+	 */
+	_updateStatus(player) {
+		if (!player) {
 			return;
 		}
-		this._name.innerText = status.name;
-		if (status.online) {
+		this._name.innerText = player.name;
+		if (player.isOnline) {
 			this.classList.add('online');
-			this.title = `${status.name} is online`;
+			this.title = `${player.name} is online`;
 		} else {
 			this.classList.remove('online');
-			this.title = `${status.name} is offline`;
+			this.title = `${player.name} is offline`;
 		}
 	};
 }
